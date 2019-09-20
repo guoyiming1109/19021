a66d58ccfd0fe14025a38b18805b3369c3bb27b2
guoyiming1109 <2530858871@qq.com>
2019-09-20 19:04:09 +0800

    修改xts

--- app/src/main/java/com/example/dell/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dell/myapplication/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        int len = 123456;
-        int max = 100;
+        String len = 123456;
+        String max = 100;
     }
 }
