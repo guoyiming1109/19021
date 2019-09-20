d04ef58777a1bbaf8669b020fcaf3a3db13f34a6
guoyiming1109 <2530858871@qq.com>
2019-09-20 18:51:32 +0800

    提交项目

---
+++ app/src/main/java/com/example/dell/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dell.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        int len = 123456;
+        int max = 100;
+        String food = "12";
+    }
+}
